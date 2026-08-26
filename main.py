5ccc1b90f088040a80ede52650f21f3213236576
javijavierojeda-hash <javijavierojeda@gmail.com>
2026-08-25 20:45:46 -0400

    Crear archivo main.py e importar la clase Vehiculo

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿from vehiculo import Vehiculo
+
+def main():
+    print("=== Bienvenido al Sistema del Taller Mecánico ===")
+    
+    # Aquí iremos agregando la lógica principal del programa
+    # Ejemplo:
+    # auto1 = Vehiculo(patente="XYZ123", modelo="Ford Fiesta", anio=2018)
+    # auto1.ingresar()
+    pass
+
+if __name__ == "__main__":
+    main()
